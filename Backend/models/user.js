5a0c7d7e895f8cc8b81a0784833c469fafefe076
= <astakhov00@gmail.com>
2023-02-16 08:01:06 -0500

    general fixes

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,13 +1,27 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  FullName: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    FullName: {
+      type: String,
+      required: true,
+    },
+    Email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    RentedBike: {
+      type: Schema.Types.ObjectId,
+      ref: "Bike",
+    },
   },
-  RentedBike: {
-    type: Schema.Types.ObjectId,
-    ref: "Bike",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 export default mongoose.model("User", UserSchema);
--- Backend/server.js
+++ Backend/server.js
@@ -11,10 +11,13 @@ const app = express();
 
 // Connect to MongoDB
 mongoose.set("strictQuery", true);
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log("DB err", err));
 
 app.use(express.json());
 
